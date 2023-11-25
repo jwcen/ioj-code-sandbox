4e65f393fc34ed856f33b6aa0c91d4022e34a3f2
jwcen <jvvcen@gmail.com>
2023-11-25 17:30:39 +0800

    feat: 远程开发

---
+++ src/main/java/com/ioj/iojcodesandbox/docker/DockerDemo.java
@@ -0,0 +1,15 @@
+package com.ioj.iojcodesandbox.docker;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.PingCmd;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+public class DockerDemo {
+
+    public static void main(String[] args) {
+        // 默认的 docker client
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+        PingCmd pingCmd = dockerClient.pingCmd();
+        pingCmd.exec();
+    }
+}
